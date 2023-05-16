b94838d35bb9edf4225c0e0f909315668fe17860
Sebastian Jacome <sebasjacome02@gmail.com>
2023-05-16 00:58:51 -0600

    Making some hotfixes

--- src/controllers/population.controllers.js
+++ src/controllers/population.controllers.js
@@ -13,9 +13,22 @@ export const getPopulation_LifeExpectancy = async (req, res) => {
     }
 }
 
-export const getPopulation_BirthDeath = async (req, res) => {
+export const getPopulation_BirthDeath_OrderByBirth = async (req, res) => {
     try{
-        const [rows] = await pool.query('select CountryCode, CountryName, DeathRate_Per1000, BirthRate_Per1000 from countrycode natural join population where BirthRate_Per1000 IS NOT NULL;')
+        const [rows] = await pool.query('select CountryCode, CountryName, DeathRate_Per1000, BirthRate_Per1000 from countrycode natural join population where BirthRate_Per1000 IS NOT NULL order by BirthRate_Per1000 desc;')
+        res.json(rows)
+    }
+    catch(error){
+        return res.status(500).json({
+            message: 'Something goes wrong',
+            error
+        })
+    }
+}
+
+export const getPopulation_BirthDeath_OrderByDeath = async (req, res) => {
+    try{
+        const [rows] = await pool.query('select CountryCode, CountryName, DeathRate_Per1000, BirthRate_Per1000 from countrycode natural join population where BirthRate_Per1000 IS NOT NULL order by DeathRate_Per1000 desc;')
         res.json(rows)
     }
     catch(error){
--- src/routes/population.routes.js
+++ src/routes/population.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { getPopulation_LifeExpectancy, getPopulation_BirthDeath } from '../controllers/population.controllers.js'
+import { getPopulation_LifeExpectancy, getPopulation_BirthDeath_OrderByBirth, getPopulation_BirthDeath_OrderByDeath } from '../controllers/population.controllers.js'
 const router = Router()
 
 router.get('/populationLife', getPopulation_LifeExpectancy)
 
-router.get('/populationBiDe', getPopulation_BirthDeath)
+router.get('/populationBiDeOrderBirth', getPopulation_BirthDeath_OrderByBirth)
+
+router.get('/populationBiDeOrderDeath', getPopulation_BirthDeath_OrderByDeath)
 
 export default router
