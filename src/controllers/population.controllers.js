df24ec36ac715b2be2cf6d4454670ee5b3f3b60b
Sebastian Jacome <sebasjacome02@gmail.com>
2023-05-16 21:17:16 -0600

    Trying some limit queries

--- src/controllers/population.controllers.js
+++ src/controllers/population.controllers.js
@@ -15,7 +15,7 @@ export const getPopulation_LifeExpectancy = async (req, res) => {
 
 export const getPopulation_BirthDeath_OrderByBirth = async (req, res) => {
     try{
-        const [rows] = await pool.query('select CountryCode, CountryName, DeathRate_Per1000, BirthRate_Per1000 from countrycode natural join population where BirthRate_Per1000 IS NOT NULL order by BirthRate_Per1000 desc limit 10;')
+        const [rows] = await pool.query('select CountryCode, CountryName, DeathRate_Per1000, BirthRate_Per1000 from countrycode natural join population where BirthRate_Per1000 IS NOT NULL order by BirthRate_Per1000 desc limit 20;')
         res.json(rows)
     }
     catch(error){
@@ -28,7 +28,7 @@ export const getPopulation_BirthDeath_OrderByBirth = async (req, res) => {
 
 export const getPopulation_BirthDeath_OrderByDeath = async (req, res) => {
     try{
-        const [rows] = await pool.query('select CountryCode, CountryName, DeathRate_Per1000, BirthRate_Per1000 from countrycode natural join population where BirthRate_Per1000 IS NOT NULL order by DeathRate_Per1000 desc limit 10;')
+        const [rows] = await pool.query('select CountryCode, CountryName, DeathRate_Per1000, BirthRate_Per1000 from countrycode natural join population where BirthRate_Per1000 IS NOT NULL order by DeathRate_Per1000 desc limit 20;')
         res.json(rows)
     }
     catch(error){
